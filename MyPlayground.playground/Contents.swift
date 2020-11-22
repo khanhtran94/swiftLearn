33dbbf44e448114c62dce64ca1aa3ee3ea51e6b3
khanhTran <khanhtrantung190894@gmail.com>
2020-11-22 09:33:30 +0700

    var, collection

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+var s: String = "a"
+s = "b"
+var myNum: Int = 5
+var myFload: Float = 32.2
+var myDouble: Double = 43
+var myBool: Bool = true
+
+var someInts = [Int]()
+someInts.append(4)
+someInts += [34]
+
+var someStrings: [String] = ["a","b"]
+
+someStrings[1]
+
