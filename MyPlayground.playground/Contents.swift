d34d98e9820e726a42459b99263e4e8ed1d3a97f
khanhTran <khanhtrantung190894@gmail.com>
2020-11-29 21:52:45 +0700

    optonals

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -37,23 +37,95 @@ import UIKit
 //
 //foodOfName["lunch"] = ["egg","cheese"]
 
+//
+//var myNum = 33
+//
+//if (myNum == 33){
+//    print(myNum)
+//} else if(myNum < 33) {
+//    print("young")
+//} else{
+//    print("old")
+//}
+//
+//myNum = 22
+//switch myNum {
+//case 33:
+//    print(myNum)
+//case 0..<33:
+//    print("young")
+//default:
+//    print("old")
+//}
+//
+//for  i in 0..<10{
+//    print(i)
+//}
+//
+//
+//print("----")
+//var numbers: [Int] = [1,23,4,22]
+//
+//for n in numbers {
+//    print(n)
+//}
+//
+//
+//print("------")
+//
+//
+//var count: Int = 2;
+//
+//while count<10 {
+//    print(count)
+//    count += 1
+//}
+//
+//
+//print("-----")
+//var j = 5
+//repeat {
+//    print(j)
+//    j += 1
+//} while j < 10
 
-var myNum = 33
 
-if (myNum == 33){
-    print(myNum)
-} else if(myNum < 33) {
-    print("young")
-} else{
-    print("old")
+func CheckCount(message: String, endCount: Int){
+    var count = 0
+    repeat {
+        print(message)
+        count += 1
+    } while count < endCount
+    
+    print("done....")
 }
 
-myNum = 22
-switch myNum {
-case 33:
-    print(myNum)
-case 0..<33:
-    print("young")
-default:
-    print("old")
+func checkOdd(number: Int) -> Bool{
+    if number % 2 == 0 {
+        return true
+    }
+    return false
+}
+
+
+CheckCount(message: "hello", endCount: 3)
+
+CheckCount(message: "World", endCount: 4)
+
+checkOdd(number: 2)
+
+print("-----")
+
+// cach nay in ra ket qua nil
+var myStr: String?
+
+print(myStr)
+
+print("------")
+
+// cach phia duoi khong in ra ket qua
+var otherStr: String? = "aa"
+
+if let myStrVal = myStr {
+    print(myStrVal)
 }
